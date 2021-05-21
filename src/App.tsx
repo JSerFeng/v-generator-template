2ce22925954f4b3ceb3a95ae71f609b828e56477
FY1010 <1114550440@qq.com>
2021-05-21 14:11:57 +0800

    模板完成

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import RenderMain from "./components/RenderMain.tsx"
+
+function App() {
+  return <div style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }}>
+    <RenderMain />
+  </div>
+}
+
+export default App
---
+++ src/components/RenderMain.tsx
@@ -0,0 +1,11 @@
+import { memo, FC } from "react"
+
+const RenderMain: FC = () => {
+  return (
+    <div className="render-main">
+
+    </div>
+  )
+}
+
+export default memo(RenderMain)
