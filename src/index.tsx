d26e3359a58678298344fc8435ede922baf60b0c
FY1010 <1114550440@qq.com>
2021-06-04 14:12:16 +0800

    fix: 修改pkgjson和移除index中store和antd样式代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store';
 
 import './index.css';
-import "antd/dist/antd.css"
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={ store }>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
